cfbcb0b7793d4fb6e40c9bc4a3d9dae149d8c354
Britta Thörnblom <15871033+brittat@users.noreply.github.com>
2025-10-28 14:58:20 +0100

    Restructure load_data.py

--- data_bootstrap/load_data.py
+++ data_bootstrap/load_data.py
@@ -2,20 +2,12 @@ import json
 import argparse
 import sys
 from MySQLdb import connect
+import pathlib
 
 from load_ingredients import ingredient_insert_statements
-from load_units import unit_insert_statements
+from data_bootstrap.load_units import unit_insert_statements
 
-def load_data(args):
-    with open(".secrets.json", "r") as f:
-        secrets = json.load(f)
-        connection_params = secrets.get(args.env)
-
-    cnx = connect(user=connection_params['user'],
-                  password=connection_params['password'],
-                  host=connection_params['host'],
-                  database='recipes')
-
+def load_data(args, cnx):
     cursor = cnx.cursor()
     for statement in ingredient_insert_statements + unit_insert_statements:
         cursor.execute(statement)
@@ -32,7 +24,17 @@ def argparser(command_line_arguments: list[str]) -> argparse.Namespace:
 
 def main(command_line_arguments):
     args = argparser(command_line_arguments=command_line_arguments)
-    load_data(args)
+    with open(str(pathlib.Path().resolve())+"/.secrets.json", "r") as f:
+        secrets = json.load(f)
+        connection_params = secrets.get(args.env)
+
+    cnx = connect(user=connection_params['user'],
+                  password=connection_params['password'],
+                  host=connection_params['host'],
+                  database='recipes')
+
+    load_data(args, cnx)
+#    a = read_data(cnx)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
