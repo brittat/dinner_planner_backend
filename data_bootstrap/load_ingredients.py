5bf6bc870b2366f9ebfb7b1c1e44403387d2d8c1
Britta Thörnblom <15871033+brittat@users.noreply.github.com>
2025-10-28 14:58:56 +0100

    Add cinnamon to ingredient bootstrap

--- data_bootstrap/load_ingredients.py
+++ data_bootstrap/load_ingredients.py
@@ -1,5 +1,6 @@
 ingredients = ["mjöl",
 "kikärter",
-"vaniljsocker"]
+"vaniljsocker",
+"kanel"]
 
 ingredient_insert_statements = [f'INSERT INTO ingredient(`ingredient`) VALUES ("{ingredient}")' for ingredient in ingredients]
