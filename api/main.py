c9c6862cb3c1b871dd5dfb9bbd10e0590d3d03a0
Britta Thörnblom <15871033+brittat@users.noreply.github.com>
2025-10-28 15:00:02 +0100

    Add endpoint for GETting all ingredients

--- api/main.py
+++ api/main.py
@@ -1,4 +1,6 @@
 from typing import Union
+import json
+from MySQLdb import connect
 
 from fastapi import FastAPI
 
@@ -13,3 +15,30 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/ingredients")
+async def read_ingredients():
+    return get_all_ingredients()
+
+
+def get_all_ingredients():
+#    with open("../.secrets.json", "r") as f:
+#        secrets = json.load(f)
+#        connection_params = secrets.get('dev')
+
+    cnx = connect(user="korvivar",
+                  password="bananarama",
+                  host="localhost",
+                  database='recipes')
+
+    cursor = cnx.cursor()
+
+    cursor.execute("DESCRIBE recipes.ingredient;")
+    cols = cursor.fetchall()
+
+    cursor.execute("SELECT * FROM recipes.ingredient;")
+    ingredients = cursor.fetchall()
+
+    return [{cols[n][0]: ingredient[n] for n in range(len(ingredient))} for ingredient in ingredients]
+
