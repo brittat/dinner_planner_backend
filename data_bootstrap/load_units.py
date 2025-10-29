44a533bce484adf0b0dde7f988e56bef0bd36d37
Britta Thörnblom <15871033+brittat@users.noreply.github.com>
2025-10-29 13:57:35 +0100

    Add more units to unit table bootstrap

--- data_bootstrap/load_units.py
+++ data_bootstrap/load_units.py
@@ -1,3 +1,3 @@
-units = ['msk', 'tsk', 'krm', 'g', 'dl', 'ml', 'l', 'kg']
+units = ['msk', 'tsk', 'krm', 'g', 'dl', 'ml', 'l', 'kg', 'st', 'gram', 'gr']
 
 unit_insert_statements = [f'INSERT INTO unit(`label`) VALUES ("{unit}")' for unit in units]
